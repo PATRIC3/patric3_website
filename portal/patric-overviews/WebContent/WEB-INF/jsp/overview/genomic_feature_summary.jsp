a338e678688b0e854c89badb43180e1361f2f137
Harry Yoo <hyun@vbi.vt.edu>
2014-10-03 11:12:24 -0400

    de-oracle for genomic feature summary

--- portal/patric-overviews/WebContent/WEB-INF/jsp/overview/genomic_feature_summary.jsp
+++ portal/patric-overviews/WebContent/WEB-INF/jsp/overview/genomic_feature_summary.jsp
@@ -1,84 +1,60 @@
 <%@ page import="java.util.*" %>
-<%@ page import="edu.vt.vbi.patric.dao.DBSummary" %>
-<%@ page import="edu.vt.vbi.patric.dao.ResultType" %>
 <%
-String cType = request.getParameter("context_type");
-String cId = request.getParameter("context_id");
-String _viewOption = request.getParameter("view");
+Map<String, Map<String, Long>> summary = (Map<String, Map<String, Long>>) request.getAttribute("summary");
 
-String context_link = "";
+String contextLink = (String) request.getAttribute("contextLink");
 
-HashMap<String,String> key = new HashMap<String, String>();
-
-if (cType!=null && cType.equals("genome") && !cId.equals("")) { 
-	key.put("genome_info_id",cId);
-	context_link = "cType=genome&amp;cId="+cId;
-}
-else if (cType!=null && cType.equals("taxon") && !cId.equals("")) { 
-	key.put("ncbi_taxon_id",cId);
-	context_link = "cType=taxon&amp;cId="+cId;
-}
-
-	key.put("view",_viewOption);
-
-if (key.size()==0 || context_link.equals("")) {
-	// no context param
-	%>Please check parameters.<%
-} else {
-	ArrayList<ResultType> items = new DBSummary().getNAFeatureSummary(key);
-	String patric, brc, refseq = null;
-	
-	%>
+String viewOption = (String) request.getAttribute("viewOption");
+%>
 	<table class="basic stripe far2x">
 	<thead>
 		<tr>
 			<th width="40%"></th>
 			<th scope="col" width="20%">PATRIC</th>
-			<th scope="col" width="20%">Legacy BRC</th>
+			<th scope="col" width="20%">BRC1</th>
 			<th scope="col" width="20%">RefSeq</th>
 		</tr>
 	</thead>
 	<tbody>
 	<%
-		String hash_key_others = "&amp;pS=20&amp;aP=1&amp;dir=ASC&amp;sort=genome_name,accession,start_max&amp;sS=All&amp;fT=";
 		boolean alt = false;
-		for ( Iterator<ResultType> iter = items.iterator(); iter.hasNext(); ) 
-		{
-			ResultType item = iter.next();
-			String name = item.get("name");
-			if (name.equalsIgnoreCase("gene") == false) {
-				
-				alt = !alt;
-				
-				if (!item.get("patric").equals("")) {
-					patric = "<a href=\"FeatureTable?"+context_link+"&amp;featuretype="+name+"&amp;annotation=PATRIC&amp;filtertype=\">"+item.get("patric")+"</a>";
-				} else {
-					patric = "0";
-				}
-				if (!item.get("brc").equals("")) {
-					brc = "<a href=\"FeatureTable?"+context_link+"&amp;featuretype="+name+"&amp;annotation=BRC&amp;filtertype=\">"+item.get("brc")+"</a>";
-				} else {
-					brc = "0";
-				}
-				if (!item.get("refseq").equals("")) {
-					refseq = "<a href=\"FeatureTable?"+context_link+"&amp;featuretype="+name+"&amp;annotation=RefSeq&amp;filtertype=\">"+item.get("refseq")+"</a>";
-				} else {
-					refseq = "0";
-				}
+
+		for (Map.Entry<String, Map<String, Long>> entry: summary.entrySet()) {
+
+            String type = entry.getKey();
+            Map<String, Long> counts = entry.getValue();
+
+            alt = !alt;
+            String patric, brc, refseq;
+
+            if (counts.containsKey("PATRIC")) {
+                patric = "<a href=\"FeatureTable?" + contextLink + "&amp;featuretype=" + type + "&amp;annotation=PATRIC&amp;filtertype=\">" + counts.get("PATRIC") + "</a>";
+            } else {
+                patric = "0";
+            }
+            if (counts.containsKey("BRC1")) {
+                brc = "<a href=\"FeatureTable?" + contextLink + "&amp;featuretype=" + type + "&amp;annotation=BRC&amp;filtertype=\">" + counts.get("BRC1") + "</a>";
+            } else {
+                brc = "0";
+            }
+            if (counts.containsKey("RefSeq")) {
+                refseq = "<a href=\"FeatureTable?" + contextLink + "&amp;featuretype=" + type + "&amp;annotation=RefSeq&amp;filtertype=\">" + counts.get("RefSeq") + "</a>";
+            } else {
+                refseq = "0";
+            }
 		%>
 			<tr <%=(alt)?"class=\"alt\"":"" %>>
-				<th scope="row"><%=item.get("name") %></th>
+				<th scope="row"><%=type %></th>
 				<td class="right-align-text"><%=patric %></td>
 				<td class="right-align-text"><%=brc %></td>
 				<td class="right-align-text last"><%=refseq %></td>
 			</tr>
 		<%
-			}
 		}
 	%>
 		<tr>
 			<td class="no-underline-links" colspan="4"><%
-				if (_viewOption.equals("full")) {
+				if (viewOption.equals("full")) {
 					%><a class="arrow-slate-e-up" href="javascript:void(0)" onClick="showShortList()">View less feature types</a><%
 				} else {
 					%><a class="arrow-slate-e-down" href="javascript:void(0)" onClick="showFullList()">View more feature types</a><%
@@ -87,4 +63,3 @@ if (key.size()==0 || context_link.equals("")) {
 		</tr>
 	</tbody>
 	</table>
-<% } %>
--- portal/patric-overviews/src/edu/vt/vbi/patric/portlets/GenomicFeatureSummaryPortlet.java
+++ portal/patric-overviews/src/edu/vt/vbi/patric/portlets/GenomicFeatureSummaryPortlet.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2014 Virginia Polytechnic Institute and State University
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,35 +15,34 @@
  ******************************************************************************/
 package edu.vt.vbi.patric.portlets;
 
+import edu.vt.vbi.patric.common.SolrCore;
+import edu.vt.vbi.patric.common.SolrInterface;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.FacetField;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.portlet.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.portlet.GenericPortlet;
-import javax.portlet.PortletException;
-import javax.portlet.PortletRequestDispatcher;
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
-import javax.portlet.ResourceRequest;
-import javax.portlet.ResourceResponse;
-import javax.portlet.UnavailableException;
+import java.net.MalformedURLException;
+import java.util.*;
 
 public class GenomicFeatureSummaryPortlet extends GenericPortlet {
 
-	protected void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException, UnavailableException {
+	private static final Logger LOGGER = LoggerFactory.getLogger(GenomicFeatureSummaryPortlet.class);
+
+	protected void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException {
 
 		response.setContentType("text/html");
 		String cType = request.getParameter("context_type");
 		String cId = request.getParameter("context_id");
-		int validContextId = -1;
 
-		if (cId != null) {
-			try {
-				validContextId = Integer.parseInt(cId);
-			}
-			catch (NumberFormatException ex) {
-			}
-		}
+		// TODO: implement redirection for p2_genome_id to new genome_id
 
-		if (cType != null && cId != null && validContextId > 0 && (cType.equals("genome") || cType.equals("taxon"))) {
+		if (cType != null && cId != null && (cType.equals("genome") || cType.equals("taxon"))) {
 			PortletRequestDispatcher prd = getPortletContext().getRequestDispatcher("/WEB-INF/jsp/overview/genomic_feature_summary_init.jsp");
 			prd.include(request, response);
 		}
@@ -56,8 +55,94 @@ public class GenomicFeatureSummaryPortlet extends GenericPortlet {
 
 	public void serveResource(ResourceRequest request, ResourceResponse response) throws PortletException, IOException {
 
-		response.setContentType("text/html");
-		PortletRequestDispatcher prd = getPortletContext().getRequestDispatcher("/WEB-INF/jsp/overview/genomic_feature_summary.jsp");
-		prd.include(request, response);
+		String contextType = request.getParameter("context_type");
+		String contextId = request.getParameter("context_id");
+		String viewOption = request.getParameter("view");
+
+		if (contextType != null && contextId != null) {
+
+			Map<String, Map<String, Long>> featureCounts = new HashMap<>();
+
+			String contextLink = null;
+			if (contextType.equals("genome")) {
+				contextLink = "cType=genome&amp;cId=" + contextId;
+			}
+			else if (contextType.equals("taxon")) {
+				contextLink = "cType=taxon&amp;cId=" + contextId;
+			}
+
+			List<String> annotations = Arrays.asList("PATRIC", "RefSeq", "BRC1");
+			int maxFeatureCount = -1;
+			Set<String> allFeatureTypes = null;
+
+			for (String annotation : annotations) {
+				Map<String, Long> featureSummary = new LinkedHashMap<>();
+				SolrQuery query = new SolrQuery();
+
+				if (contextType.equals("taxon")) {
+					if (!viewOption.equals("full")) {
+						query.setQuery("annotation:" + annotation + " AND feature_type:(CDS OR *RNA)");
+					}
+					else {
+						query.setQuery("annotation:" + annotation);
+					}
+					query.setFilterQueries(SolrCore.GENOME.getSolrCoreJoin("genome_id", "genome_id", "taxon_lineage_ids:" + contextId));
+				}
+				else {
+					if (!viewOption.equals("full")) {
+						query.setQuery("annotation:" + annotation + " AND genome_id:" + contextId + " AND feature_type:(CDS OR *RNA)");
+					}
+					else {
+						query.setQuery("annotation:" + annotation + " AND genome_id:" + contextId);
+					}
+				}
+
+				query.setRows(0).setFacet(true).setFacetMinCount(1).addFacetField("feature_type");
+
+				try {
+					SolrInterface solr = new SolrInterface();
+					solr.setCurrentInstance(SolrCore.FEATURE);
+
+					QueryResponse qr = solr.getServer().query(query);
+
+					FacetField facetField = qr.getFacetField("feature_type");
+
+					for (FacetField.Count facetValue : facetField.getValues()) {
+						featureSummary.put(facetValue.getName(), facetValue.getCount());
+					}
+
+					featureCounts.put(annotation, featureSummary);
+					if (featureSummary.size() > maxFeatureCount) {
+						allFeatureTypes = featureSummary.keySet();
+						maxFeatureCount = featureCounts.size();
+					}
+
+				}
+				catch (MalformedURLException | SolrServerException e) {
+					LOGGER.error(e.getMessage(), e);
+				}
+			}
+
+			// transpose arrays
+			Map<String, Map<String, Long>> summary = new LinkedHashMap<>();
+			assert allFeatureTypes != null;
+			for (String type : allFeatureTypes) {
+				Map<String, Long> sum = new HashMap<>();
+				for (String annotation : annotations) {
+					if (featureCounts.containsKey(annotation) && featureCounts.get(annotation).containsKey(type)) {
+						sum.put(annotation, featureCounts.get(annotation).get(type));
+					}
+				}
+				summary.put(type, sum);
+			}
+
+			response.setContentType("text/html");
+			request.setAttribute("summary", summary);
+			request.setAttribute("contextLink", contextLink);
+			request.setAttribute("viewOption", viewOption);
+
+			PortletRequestDispatcher prd = getPortletContext().getRequestDispatcher("/WEB-INF/jsp/overview/genomic_feature_summary.jsp");
+			prd.include(request, response);
+		}
 	}
 }
