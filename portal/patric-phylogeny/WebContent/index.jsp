1aa249588a18b15222ec0fc5995c6f30ae1a11cb
Harry Yoo <hyun@vbi.vt.edu>
2014-10-06 17:14:42 -0400

    p3 scheme, de-oracle

--- portal/patric-phylogeny/WebContent/index.jsp
+++ portal/patric-phylogeny/WebContent/index.jsp
@@ -1,57 +1,48 @@
-<%@ page session="true" %>
-<%@ page import="java.util.*" %>
-<%@ page import="edu.vt.vbi.patric.dao.DBShared" %>
-<%@ page import="edu.vt.vbi.patric.dao.DBSummary" %>
-<%@ page import="edu.vt.vbi.patric.dao.ResultType" %>
-<%
-String cType = request.getParameter("context_type");
-String cId = request.getParameter("context_id");
-String hostName = request.getServerName();
+<%@ page import="java.util.*" %><%
+
 boolean hasMultipleOrders = false;
 
-if ((cType.equals("taxon") || cType.equals("genome")) && cId!=null) {
-	String taxonId = "";
+List<Map<String, Object>> orderList = (List<Map<String, Object>>) request.getAttribute("orderList");
+int taxonId = (Integer) request.getAttribute("taxonId");
 
-	if (cType.equals("genome")) {
-		DBShared conn_shared = new DBShared();
-		ResultType names = (ResultType) conn_shared.getNamesFromGenomeInfoId(cId);
-		taxonId = names.get("ncbi_taxon_id");
-	} else {
-		taxonId = cId;
-	}
-	
-	DBSummary conn_summary = new DBSummary();
-	List<ResultType> items = conn_summary.getOrderInTaxonomy(Integer.parseInt(taxonId));
-	ResultType item = null;
-//	Iterator<ResultType> iter = null;
-	if (items.size() > 1) {
-		hasMultipleOrders = true;
-	}
+if (!orderList.isEmpty()) {
+
+    if (orderList.size() > 1) {
+        hasMultipleOrders = true;
+    }
+
 	%>
-	<!--[if IE]><script type="text/javascript" src="<%=request.getContextPath() %>/js/excanvas_r73.js"></script><![endif]-->
+<%--	<!--[if IE]><script type="text/javascript" src="<%=request.getContextPath() %>/js/excanvas_r73.js"></script><![endif]-->--%>
 	<script type="text/javascript" src="<%=request.getContextPath() %>/js/genomeMaps.js"></script>
 	<script type="text/javascript" src="<%=request.getContextPath() %>/js/TreeNav.js"></script>
-	
-	<p>Chose an order to view its mulit-gene phylogenetic tree in either phylogram or cladogram format. 
-			Use controls above the tree to show or hide support values, and to color the tree according to genus or species.  
-			Click on a species name to visit that species overview page.  
+
+	<p>Chose an order to view its mulit-gene phylogenetic tree in either phylogram or cladogram format.
+			Use controls above the tree to show or hide support values, and to color the tree according to genus or species.
+			Click on a species name to visit that species overview page.
 			To learn more about interacting with trees and the method used to create them, please see <a href="http://enews.patricbrc.org/phylogeny-faqs/" target="_blank">Phylogeny FAQs</a>.
 	</p>
-	
+
 	<div id="tree_controls">
 		<div> Show phylogentic tree for the order
 		<% if (hasMultipleOrders) { %>
 			<select id="tree_select_list" onchange="selectChange()">
-			<% for (int i=0; i<items.size(); i++) { %>
-				<option value="<%=items.get(i).get("ncbi_tax_id")%>" <%=(i==0)?"selected='selected'":"" %>><%=items.get(i).get("name")%></option>
-			<% } %>
+			<%
+			int i = 0;
+			for (Map<String, Object> node: orderList) {
+			    %><option value="<%=node.get("taxonId")%>" <%=(i==0)?"selected='selected'":"" %>><%=node.get("name")%></option><%
+			}
+			i++;
+			%>
 			</select>
-		<% } else { %>
-			<%=items.get(0).get("name") %>
-			<input type="hidden" id="tree_select_list" value="<%=items.get(0).get("ncbi_tax_id") %>"/>
-		<% } %>
+		<%
+		} else {
+            Map<String, Object> node = orderList.get(0);
+
+            %><%=node.get("name") %><input type="hidden" id="tree_select_list" value="<%=node.get("taxonId") %>"/><%
+		}
+		%>
 		</div>
-		
+
 		<div style="float:left;width:25%">
 			<span style="float:left">Draw tree as</span>
 			<div style="margin-left:85px">
@@ -59,7 +50,7 @@ if ((cType.equals("taxon") || cType.equals("genome")) && cId!=null) {
 				<input type="radio" name="tree_type" id="tree_type_cladogram" value="cladogram" onclick="cladogram()" checked="checked" /> <label for="tree_type_cladogram">cladogram</label>
 			</div>
 		</div>
-		
+
 		<div style="float:left;width:25%">
 			<div style="float:left">Color tree by</div>
 			<div style="margin-left:85px">
@@ -68,7 +59,7 @@ if ((cType.equals("taxon") || cType.equals("genome")) && cId!=null) {
 				<input type="radio" name="tree_color" id="tree_color_off" value="off" onclick="colorOff()" /> <label for="tree_color_off">off</label>
 			</div>
 		</div>
-		
+
 		<div style="float:left;width:25%">
 			<div style="float:left">Turn support values</div>
 			<div style="margin-left:120px">
@@ -76,23 +67,23 @@ if ((cType.equals("taxon") || cType.equals("genome")) && cId!=null) {
 				<input type="radio" name="tree_supports" id="tree_supports_off" value="off" onclick="hideSupports()" /> <label for="tree_supports_off">off</label>
 			</div>
 		</div>
-		
+
 		<div style="float:left;width:25%">
-			<a id="nwk_link" href="/patric-phylogeny/resources/readNWK.jsp?taxonID=<%=items.get(0).get("ncbi_tax_id") %>" target="_blank">Download tree as Newick file</a>
+			<a id="nwk_link" href="/patric-phylogeny/resources/readNWK.jsp?taxonID=<%=taxonId %>" target="_blank">Download tree as Newick file</a>
 		</div>
 		<div>
 			<input type="button" class="button" title="Add Genome(s) to Workspace" value="Add Genome(s) to Workspace" onclick="AddToCart()">
 		</div>
-		
+
 		<div class="clear"></div>
 	</div>
-	
+
 	<div id="TreeNavDiv" style="position:relative">
 	<div id="TreeNavDiv_checkbox" style="width:100%;"></div></div>
 
-<%	
-} else { 
-	%>wrong parameter<%	
+<%
+} else {
+	%>wrong parameter<%
 }
 %>
 <script type="text/javascript" src="/patric/static/phylogeny/outgroups.js"></script>
@@ -128,21 +119,21 @@ function treeNav_init() {
 function updateConfiguration() {
 
     if (Ext.getDom("tree_type_phylogram").checked) {
-    	phylogram();
+        phylogram();
     } else {
-    	cladogram();
+        cladogram();
     }
 
     if (Ext.getDom("tree_color_genus").checked) {
-    	colorGenus();
+        colorGenus();
     } else if (Ext.getDom("tree_color_speices").checked) {
-    	colorSpecies();
+        colorSpecies();
     } else {
-    	colorOff();
+        colorOff();
     }
 
     if (Ext.getDom("tree_supports_on").checked) {
-    	showSupports();
+        showSupports();
     } else {
         hideSupports();
     }
@@ -202,7 +193,7 @@ function updateTree(tID) {
 			treeNav.setTree(rs.responseText);
 			var outgrp = outgroups[orderMap[tID]];
 			//console.log(tID, orderMap[tID], outgrp);
-			
+
 			if (outgrp.length > 0) {
 				//console.log("updating outgroup:"+outgrp);
 				treeNav.setOutgroup(outgrp);
@@ -221,22 +212,21 @@ function AddToCart(){
 }
 
 Ext.onReady(function () {
-	
+
 	var Page = $Page, btnGroupPopupSave = Page.getCartSaveButton();
-	
-	
+
 	treeNav_init();
-	
+
 	if (Ext.get("tabs_phylogeny")!=null) {
 		Ext.get("tabs_phylogeny").addCls("sel");
 	}
-	
+
 	if(btnGroupPopupSave != null){
 		btnGroupPopupSave.on('click', function(){
 			saveToGroup(selectedGenomes, "Genome");
 			clearSelectedGenomes();
 		});
-	}	
+	}
 });
 //]]>
 </script>
--- portal/patric-phylogeny/src/edu/vt/vbi/patric/portlets/PhylogeneticTree.java
+++ portal/patric-phylogeny/src/edu/vt/vbi/patric/portlets/PhylogeneticTree.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2014 Virginia Polytechnic Institute and State University
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,64 +15,159 @@
  ******************************************************************************/
 package edu.vt.vbi.patric.portlets;
 
+import edu.vt.vbi.patric.beans.Genome;
+import edu.vt.vbi.patric.common.SiteHelper;
+import edu.vt.vbi.patric.common.SolrCore;
+import edu.vt.vbi.patric.common.SolrInterface;
+import org.apache.commons.lang.StringUtils;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.portlet.*;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.List;
-
-import javax.portlet.GenericPortlet;
-import javax.portlet.PortletException;
-import javax.portlet.PortletRequestDispatcher;
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
-import javax.portlet.UnavailableException;
-
-import edu.vt.vbi.patric.common.SiteHelper;
-import edu.vt.vbi.patric.dao.DBSummary;
-import edu.vt.vbi.patric.dao.ResultType;
+import java.net.MalformedURLException;
+import java.util.*;
 
 public class PhylogeneticTree extends GenericPortlet {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.portlet.GenericPortlet#doView(javax.portlet.RenderRequest, javax.portlet.RenderResponse)
-	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(PhylogeneticTree.class);
+
+	SolrInterface solr;
+
 	@Override
 	public void init() throws PortletException {
 		super.init();
 
 		// update genome-id mapping cache
 		try {
-			HashMap<String, String> key = new HashMap<String, String>();
-			key.put("ncbi_taxon_id", "2");
-			key.put("data_source", "");
+			solr = new SolrInterface();
+			solr.setCurrentInstance(SolrCore.GENOME);
 
-			DBSummary conn = new DBSummary();
-			List<ResultType> list = conn.getGenomeListByTaxon(key, null, 0, -1);
+			SolrQuery query = new SolrQuery("*:*");
+			query.setFields("genome_name,genome_id");
+
+			QueryResponse qr = solr.getServer().query(query);
+			List<Genome> genomes = qr.getBeans(Genome.class);
+
 			StringBuilder sb = new StringBuilder();
 			sb.append("var genomeMap = new Array();\n");
-			for (int i = 0; i < list.size(); i++) {
-				sb.append("genomeMap[\"" + list.get(i).get("genome_name").replaceAll("[\\s\\(\\)\\:\\[\\],]+", "_") + "\"] = "
-						+ list.get(i).get("genome_info_id") + ";\n");
+
+			for (Genome genome : genomes) {
+				sb.append("genomeMap[\"").append(genome.getGenomeName().replaceAll("[\\s\\(\\)\\:\\[\\],]+", "_")).append("\"] = \"")
+						.append(genome.getId()).append("\";\n");
 			}
 
 			PrintWriter out = new PrintWriter(new FileWriter(getPortletContext().getRealPath("/js/genomeMaps.js")));
 			out.println(sb.toString());
 			out.close();
 		}
+		catch (MalformedURLException | SolrServerException e) {
+			LOGGER.error(e.getMessage(), e);
+		}
 		catch (Exception e) {
-			e.printStackTrace();
+			LOGGER.error(e.getMessage(), e);
 		}
 	}
 
-	protected void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException, UnavailableException {
+	protected void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException {
 		response.setContentType("text/html");
 
 		new SiteHelper().setHtmlMetaElements(request, response, "Phylogeny");
 
-		PortletRequestDispatcher prd = getPortletContext().getRequestDispatcher("/index.jsp");
-		prd.include(request, response);
+		List<Integer> phylogenyOrderIds = Arrays
+				.asList(2037, 1385, 80840, 213849, 51291, 186802, 91347, 186826, 118969, 356, 766, 136, 72273, 135623);
+
+		String contextType = request.getParameter("context_type");
+		String contextId = request.getParameter("context_id");
+
+		if (contextType != null && contextId != null) {
+
+			List<Map<String, Object>> orderList = new ArrayList<>();
+			int taxonId = 0;
+
+			try {
+				if (contextType.equals("genome")) {
+					solr.setCurrentInstance(SolrCore.GENOME);
+					SolrQuery query = new SolrQuery("genome_id:" + contextId);
+					query.setFields("taxonId");
+
+					QueryResponse qr = solr.getServer().query(query);
+					List<Genome> genomes = qr.getBeans(Genome.class);
+
+					for (Genome genome : genomes) {
+						taxonId = genome.getTaxonId();
+					}
+				}
+				else {
+					taxonId = Integer.parseInt(contextId);
+				}
+
+				solr.setCurrentInstance(SolrCore.TAXONOMY);
+
+				// Step1. has Order in lineage?
+				SolrQuery query = new SolrQuery("taxon_id:" + taxonId + " AND lineage_ranks:order");
+				query.setFields("lineage_ids,lineage_names,lineage_ranks");
+				QueryResponse qr = solr.getServer().query(query);
+
+				SolrDocumentList sdl = qr.getResults();
+				for (SolrDocument doc : sdl) {
+					List<Integer> txIds = (List<Integer>) doc.get("lineage_ids");
+					List<String> txNames = (List<String>) doc.get("lineage_names");
+					List<String> txRanks = (List<String>) doc.get("lineage_ranks");
+
+					for (Integer txId : txIds) {
+						int idx = txIds.indexOf(txId);
+
+						if (txRanks.get(idx).equals("order")) {
+							Map<String, Object> node = new HashMap<>();
+							node.put("taxonId", txId);
+							node.put("name", txNames.get(idx));
+
+							orderList.add(node);
+						}
+					}
+				}
+
+				if (orderList.isEmpty()) {
+					// no rank Order found in lineage, then,
+					// Step2. has Order rank in descendants
+					query = new SolrQuery(
+							"lineage_ids:" + taxonId + " AND taxon_rank:order AND taxon_id:(" + StringUtils.join(phylogenyOrderIds, " OR ") + ")");
+					query.setFields("taxon_id,taxon_name,taxon_rank");
+					query.setRows(100);
+					qr = solr.getServer().query(query);
+
+					sdl = qr.getResults();
+					for (SolrDocument doc : sdl) {
+
+						if (doc.get("taxon_rank").equals("order")) {
+							Map<String, Object> node = new HashMap<>();
+							node.put("taxonId", (Integer) doc.get("taxon_id"));
+							node.put("name", doc.get("taxon_name").toString());
+
+							orderList.add(node);
+						}
+					}
+				}
+			}
+			catch (MalformedURLException | SolrServerException e) {
+				LOGGER.error(e.getMessage(), e);
+			}
+
+			LOGGER.trace("{}", orderList);
+
+			request.setAttribute("orderList", orderList);
+			request.setAttribute("taxonId", taxonId);
+
+			PortletRequestDispatcher prd = getPortletContext().getRequestDispatcher("/index.jsp");
+			prd.include(request, response);
+		}
 	}
 }
