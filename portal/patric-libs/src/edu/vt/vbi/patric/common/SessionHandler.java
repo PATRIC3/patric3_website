ba60c18083c041337c6157b72efd1ad83f65942a
Harry Yoo <hyun@vbi.vt.edu>
2015-03-19 17:39:24 -0400

    SessionHandler to be thread-safe using JedisPool

--- portal/patric-libs/src/edu/vt/vbi/patric/common/SessionHandler.java
+++ portal/patric-libs/src/edu/vt/vbi/patric/common/SessionHandler.java
@@ -19,6 +19,9 @@ package edu.vt.vbi.patric.common;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.exceptions.JedisException;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,32 +31,15 @@ public class SessionHandler {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(SessionHandler.class);
 
-	private static SessionHandler instance;
-
-	private Jedis jedis;
-
 	public static String PREFIX = "JBSess";
+
+	private static volatile SessionHandler instance;
 
 	private final int expiration = 60 * 60; // 1 hour
 
+	private JedisPool pool;
+
 	private SessionHandler() {
-
-		try (InputStream is = getClass().getClassLoader().getResourceAsStream("redis_config.properties")) {
-			Properties prop = new Properties();
-			if (is != null) {
-				prop.load(is);
-				String host = prop.getProperty("host", "localhost");
-				int port = Integer.parseInt(prop.getProperty("port", "6379"));
-				//				int database = Integer.parseInt(prop.getProperty("database", "1"));
-				int timeout = Integer.parseInt(prop.getProperty("timeout", "60"));
-
-				jedis = new Jedis(host, port, timeout);
-				LOGGER.debug("connecting Redis Server: {},{},{}", host, port, timeout);
-			}
-		}
-		catch (IOException e) {
-			LOGGER.error(e.getMessage(), e);
-		}
 	}
 
 	public static SessionHandler getInstance() {
@@ -61,18 +47,55 @@ public class SessionHandler {
 			synchronized (SessionHandler.class) {
 				if (null == instance) {
 					instance = new SessionHandler();
+					instance.connect();
 				}
 			}
 		}
 		return instance;
 	}
 
+	private void connect() {
+
+		try (InputStream is = getClass().getClassLoader().getResourceAsStream("redis_config.properties")) {
+			Properties prop = new Properties();
+			if (is != null) {
+				prop.load(is);
+				String host = prop.getProperty("host", "localhost");
+				int port = Integer.parseInt(prop.getProperty("port", "6379"));
+				int timeout = Integer.parseInt(prop.getProperty("timeout", "60"));
+				String password = prop.getProperty("password");
+				int database = Integer.parseInt(prop.getProperty("database", "0"));
+
+				pool = new JedisPool(new JedisPoolConfig(), host, port, timeout, password, database);
+				LOGGER.debug("connecting Redis Server: {},{},{},{},{}", host, port, timeout, password, database);
+			}
+		}
+		catch (IOException e) {
+			LOGGER.error(e.getMessage(), e);
+		}
+	}
+
 	public String get(String key) {
-		return jedis.get(key);
+		try {
+			Jedis jedis = pool.getResource();
+			String val = jedis.get(key);
+			pool.returnResource(jedis);
+			return val;
+		}
+		catch (JedisException e) {
+			LOGGER.error(e.getMessage(), e);
+			return null;
+		}
 	}
 
 	public void set(String key, String val) {
-		// session.set(key, val);
-		jedis.setex(key, expiration, val);
+		try {
+			Jedis jedis = pool.getResource();
+			jedis.setex(key, expiration, val);
+			pool.returnResource(jedis);
+		}
+		catch (JedisException e) {
+			LOGGER.error(e.getMessage(), e);
+		}
 	}
 }
