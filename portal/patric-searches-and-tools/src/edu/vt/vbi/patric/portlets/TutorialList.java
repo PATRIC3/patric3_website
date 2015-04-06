6ab16105e44756ad545584d820deb2702b0a6beb
Harry Yoo <hyun@vbi.vt.edu>
2015-04-06 12:13:55 -0400

    code cleanup

--- portal/patric-searches-and-tools/src/edu/vt/vbi/patric/portlets/TutorialList.java
+++ portal/patric-searches-and-tools/src/edu/vt/vbi/patric/portlets/TutorialList.java
@@ -1,44 +1,41 @@
-/*******************************************************************************
+/**
+ * ****************************************************************************
  * Copyright 2014 Virginia Polytechnic Institute and State University
- * 
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ * ****************************************************************************
+ */
 package edu.vt.vbi.patric.portlets;
 
-import java.io.*;
-
-import javax.portlet.GenericPortlet;
-import javax.portlet.PortletException;
-import javax.portlet.PortletRequestDispatcher;
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
-import javax.portlet.ResourceRequest;
-import javax.portlet.ResourceResponse;
-import javax.portlet.UnavailableException;
-
 import org.json.simple.JSONObject;
-import org.json.simple.parser.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.portlet.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.URL;
 
 public class TutorialList extends GenericPortlet {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.portlet.GenericPortlet#doView(javax.portlet.RenderRequest, javax.portlet.RenderResponse)
-	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(Downloads.class);
+
 	@Override
-	protected void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException, UnavailableException {
+	protected void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException {
 		response.setContentType("text/html");
 		response.setTitle("Tutorial List");
 
@@ -51,32 +48,23 @@ public class TutorialList extends GenericPortlet {
 
 		JSONObject jsonResult = new JSONObject();
 
-		URL jsonURL = new URL("http://enews.patricbrc.org/tutorial_landing/tutorial-landing-page-1-3/?req=passphrase");
+		URL url = new URL("http://enews.patricbrc.org/tutorial_landing/tutorial-landing-page-1-3/?req=passphrase");
 
-		InputStream dh = null;
-		int cp;
-
-		StringBuilder json_builder = new StringBuilder();
-		try {
-			dh = jsonURL.openStream();
-			while ((cp = dh.read()) != -1) {
-				json_builder.append((char) cp);
-			}
-			try {
-				jsonResult.put("tutorials", (JSONObject) new JSONParser().parse(json_builder.toString()));
-			}
-			catch (Exception e) {
-			}
+		StringBuilder sb = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+			sb.append(br.readLine());
 		}
-		finally {
-			if (dh != null) {
-				dh.close();
-			}
+
+		try {
+			jsonResult.put("tutorials", new JSONParser().parse(sb.toString()));
+		}
+		catch (ParseException e) {
+			LOGGER.error(e.getMessage(), e);
 		}
 
 		response.setContentType("application/json");
 		PrintWriter writer = response.getWriter();
-		writer.write(jsonResult.toString());
+		jsonResult.writeJSONString(writer);
 		writer.close();
 	}
 }
