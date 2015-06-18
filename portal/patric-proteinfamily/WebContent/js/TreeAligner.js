27f900a3451c1bfcef97d2ef47faa9069b0888a4
Andrew Warren <aswarren@gmail.com>
2015-06-18 16:31:13 -0400

    updating requires msa

--- portal/patric-proteinfamily/WebContent/js/TreeAligner.js
+++ portal/patric-proteinfamily/WebContent/js/TreeAligner.js
@@ -112,6 +112,9 @@ function insertMSAJS(windowID, ajaxHttp) {
 
     // this is a way how you use a bundled file parser
     var msa = require("msa");
+    var newick = require("biojs-io-newick");
+    var mt = require("msa-tnt");
+
     var msa_models = {
             seqs: msa.io.clustal.parse(inputData.clustal),
             tree: newick.parse_newick(inputData.tree)
